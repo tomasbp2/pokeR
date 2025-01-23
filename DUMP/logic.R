152994687a815103118cc4825871ae6df224ed67
ArthurGontijo <arthur.silveira2004@gmail.com>
2025-01-23 15:54:01 -0300

    adicionando fulllhouse e consertando algumas coisas para o straight

--- DUMP/logic.R
+++ DUMP/logic.R
@@ -51,15 +51,34 @@ check_combination <- function(cards) {
   suits_counts = table(suits)
   mat_counts = table(ranks,suits)
 
-  print(rank_counts)
-  print(suits_counts)
-  print(mat_counts)
+  # Se não fizer assim, sequências com números repetidos nao seriam considera-
+  # das straight
+  sorted = ranks |> unique() |> sort()
 
-  sorted = sort(ranks)
 
-  for(i in 1:3){
-    if( all.equal(seq(sorted[i]:sorted[i+4]), sorted[i:(i+4)]) ){
-      if(any(colSums(mat_counts[i:(i+4),]) == 5)){
+  # Uma mão tipo 3 3 3 2 2 4 4 bugaria
+  if(length(sorted) >= 5) {
+    for(i in 1:3){
+      if(all.equal(seq(sorted[i], sorted[i + 4]), sorted[i:(i + 4)]) == TRUE){
+
+        # Do jeito anterior dava erro se i + 4 fosse maior que o tamanho da mat
+        # Fazendo dessa forma, garantimos que estamos acessando o rank "i" e não
+        # a linha "i"
+        ranks_to_check <- as.character(i:(i + 4))
+
+        if(any(colSums(mat_counts[ranks_to_check,]) == 5)){
+          return("straight flush")
+        } else {
+          return("straight")
+        }
+      }
+    }
+
+    # Quando A é a maior carta:
+    if((sorted[1] == 1) && all(10:13 %in% sorted)){
+      ranks_to_check <- c("1", "10", "11", "12", "13")
+
+      if(any(colSums(mat_counts[ranks_to_check,]) == 5)){
         return("straight flush")
       } else {
         return("straight")
@@ -72,6 +91,9 @@ check_combination <- function(cards) {
   }
   if(sum(rank_counts == 4) == 1){
     return("farofakind")
+  }
+  if(sum(rank_counts == 3) == 1 && sum(rank_counts == 2) == 1){
+    return("full house")
   }
   if(sum(rank_counts == 3) == 1){
     return("trinca")
@@ -87,11 +109,3 @@ check_combination <- function(cards) {
 
 comb
 print(check_combination(comb))
-
-
-# if((sort(ranks)[i+4] - sort(ranks)[i] == 4) && sort(ranks)[i+1] == (sort(ranks)[i]+1)){
-#   if(any(colSums(mat_counts[i:(i+4),])) == 5){
-#     return("straight flush")
-#   } else
-#     return("straight")
-# }
